20f3ade70699eb3044ba94e4fb5e7572d4449467
khoirunnasikin95 <khoirunnasikin95@gmail.com>
2025-07-06 01:33:33 +0700

    Simpan perubahan sebelum rebase

--- kas_gc3k/settings.py
+++ kas_gc3k/settings.py
@@ -185,6 +185,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Tambahkan ini
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
